7a8da572cc3584a9742b545f158478b8e46ffd63
Manpreet2001 <67718334+Manpreet2001@users.noreply.github.com>
2020-07-18 13:20:58 -0700

    I am going to add file for attacker interface

---
+++ Attacker.java
@@ -0,0 +1,9 @@
+public interface Attacker
+{
+public int getkillCount();// method to get count over kills
+public int gethealth();// method to get health
+public void sethealth();// method to set health
+public boolean attack(Animal animal);// attack method to check condition for kills
+
+
+}
