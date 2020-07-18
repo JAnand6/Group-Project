7e5463b11ca110b2e74c8b1b7ba18bd6aae7a510
JAnand6 <68035967+JAnand6@users.noreply.github.com>
2020-07-18 11:14:12 -0700

    Create WaterFowl.java

---
+++ WaterFowl.java
@@ -0,0 +1,14 @@
+public class WaterFowl extends Animal
+{
+     
+  public void fly()
+  {
+    System.out.println(getName() + " is flying");
+  }
+  
+  public void swim()
+  {
+    System.out.println(getName() + " is swimming");
+  }
+  
+}
