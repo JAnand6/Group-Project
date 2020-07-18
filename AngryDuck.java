a2e2badfbfb6b6e5b6ce769f1ba684d27d6a8080
JAnand6 <68035967+JAnand6@users.noreply.github.com>
2020-07-18 11:09:17 -0700

    Create AngryDuck.java

---
+++ AngryDuck.java
@@ -0,0 +1,14 @@
+public class AngryDuck extends WaterFowl 
+{
+     
+  public AngryDuck()
+ {
+   Animal angryDuck = new Animal();
+   
+   angryDuck.setName("AngryDuck");
+   
+   angryDuck.setColor("White");
+   System.out.println(angryDuck);
+ }
+  
+}
